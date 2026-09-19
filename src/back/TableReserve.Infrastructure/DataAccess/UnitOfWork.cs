bb49bfc4df56cf355446d95d5dc0c2bbfe90136e
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-19 11:45:34 -0300

    feat(infra): implement UnitOfWork
    - Implement IUnitOfWork interface using EF Core's SaveChangesAsync.
    - Provide atomic transaction commit functionality across repositories sharing TableReserveDbContext.

---
+++ src/back/TableReserve.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using TableReserve.Domain.Repositories;
+
+namespace TableReserve.Infrastructure.DataAccess;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly TableReserveDbContext _dbContext;
+    public UnitOfWork(TableReserveDbContext dbContext) => _dbContext = dbContext;
+    public async Task Commit(CancellationToken cancellationToken) => await _dbContext.SaveChangesAsync(cancellationToken);
+}
