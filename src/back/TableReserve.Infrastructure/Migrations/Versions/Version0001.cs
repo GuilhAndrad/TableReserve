1547d582b05674d9d511eee046851c098e1180e7
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-19 20:32:07 -0300

    feat(infra): add Version0001 migration to create Users table
    - Implement ForwardOnlyMigration using FluentMigrator.
    - Create Users table with Id, Active, Name, Email, Password, and Role columns.
    - Configure primary key, constraints, uniqueness, and defaults.

---
+++ src/back/TableReserve.Infrastructure/Migrations/Versions/Version0001.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace TableReserve.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.Add_Users_Table, "Creating Users table")]
+public class Version0001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("Name").AsString(250).NotNullable()
+            .WithColumn("Email").AsString(250).NotNullable().Unique()
+            .WithColumn("Password").AsString(255).NotNullable()
+            .WithColumn("Role").AsString(50).NotNullable();
+    }
+}
