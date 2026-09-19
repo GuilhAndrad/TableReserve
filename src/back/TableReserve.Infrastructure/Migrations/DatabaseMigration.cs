a6e1a7a0a1243f44ec6a203405adb3d96487e99e
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-19 20:33:10 -0300

    feat(infra): add DatabaseMigration runner utility
    - Create DatabaseMigration static utility to invoke IMigrationRunner.
    - Trigger ListMigrations and MigrateUp during application initialization.

---
+++ src/back/TableReserve.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TableReserve.Infrastructure.Migrations;
+
+internal class DatabaseMigration
+{
+    public static void RunnerMigrations(IServiceProvider serviceProvider)
+    {
+        IMigrationRunner runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+
+        runner.MigrateUp();
+    }
+}
