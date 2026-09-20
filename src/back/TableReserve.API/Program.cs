4738e82da748195db4506afd686c6ad356dafdd2
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-20 19:49:00 -0300

    feat(api): wire dependency injection and migration runner in Program
    - Invoke AddInfrastructure extension to register core services.
    - Register IHttpContextAccessor and HttpContextTokenProvider.
    - Implement ExecuteMigrations step to run FluentMigrator schema updates on startup.

--- src/back/TableReserve.API/Program.cs
+++ src/back/TableReserve.API/Program.cs
@@ -1,8 +1,20 @@
+using TableReserve.API.Token;
+using TableReserve.Domain.Security.Tokens;
+using TableReserve.Infrastructure;
+using TableReserve.Infrastructure.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddScoped<IAccessTokenProvider, HttpContextTokenProvider>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -20,4 +32,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await ExecuteMigrations();
+
 app.Run();
+
+async Task ExecuteMigrations()
+{
+    await using AsyncServiceScope scope = app.Services.CreateAsyncScope();
+    DatabaseMigration.RunnerMigrations(scope.ServiceProvider);
+}
