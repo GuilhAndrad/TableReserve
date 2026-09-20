2d532c96ec117fdfed7e7732c6c35848b5fd6666
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-20 19:47:12 -0300

    feat(api): implement HttpContextTokenProvider
    - Implement IAccessTokenProvider interface using IHttpContextAccessor.
    - Extract JWT authorization token from HTTP request headers.
    - Strip the 'Bearer ' prefix to return the raw token string.

---
+++ src/back/TableReserve.API/Token/HttpContextTokenProvider.cs
@@ -0,0 +1,17 @@
+﻿using TableReserve.Domain.Security.Tokens;
+
+namespace TableReserve.API.Token;
+
+internal sealed class HttpContextTokenProvider : IAccessTokenProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public HttpContextTokenProvider(IHttpContextAccessor httpContextAccessor) => _httpContextAccessor = httpContextAccessor;
+
+    public string GetToken()
+    {
+        string accessToken = _httpContextAccessor.HttpContext!.Request.Headers.Authorization.ToString();
+
+        return accessToken["Bearer ".Length..];
+    }
+}
