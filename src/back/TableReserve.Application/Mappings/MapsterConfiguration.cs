09d348af4ecee8ec6d9acbdbd7f482a318042f02
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-22 20:16:03 -0300

    feat(application): add MapsterConfiguration to ignore plain-text password mapping
    - Create MapsterConfiguration static class to centralize object mapping rules.
    - Configure TypeAdapterConfig to ignore the Password destination property when mapping RegisterUserRequest to User.
    - Enforce cryptographic hashing workflow by preventing accidental plain-text assignment.

---
+++ src/back/TableReserve.Application/Mappings/MapsterConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Mapster;
+using TableReserve.Communication.Requests;
+using TableReserve.Domain.Entities;
+
+namespace TableReserve.Application.Mappings;
+
+internal static class MapsterConfiguration
+{
+    internal static void Configure()
+    {
+        TypeAdapterConfig<RegisterUserRequest, User>
+            .NewConfig()
+            .Ignore(dest => dest.Password);
+    }
+}
