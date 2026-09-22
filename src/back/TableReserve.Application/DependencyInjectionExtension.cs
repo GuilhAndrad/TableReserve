ce0eb5b4e5d287fa6e7d7278d0d99e4529ceb412
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-22 20:19:44 -0300

    feat(application): add DependencyInjectionExtension for application wiring
    - Create DependencyInjectionExtension to manage application layer service registrations.
    - Trigger central Mapster configurations via MapsterConfiguration.Configure().
    - Register IRegisterUser and RegisterUser use case dependency within the scoped container.

---
+++ src/back/TableReserve.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TableReserve.Application.Mappings;
+
+namespace TableReserve.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        MapsterConfiguration.Configure();
+
+        services.AddUseCases();
+    }
+
+    private static void AddUseCases(this IServiceCollection services)
+    {
+        services.AddScoped<UseCases.User.RegisterAccount.IRegisterUser, UseCases.User.RegisterAccount.RegisterUser>();
+    }
+}
