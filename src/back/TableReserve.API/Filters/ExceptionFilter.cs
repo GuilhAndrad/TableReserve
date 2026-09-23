b045e4b7a8e5d0d9af7043bbdb5160ae7673ba4f
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-23 09:27:27 -0300

    feat(api): implement ExceptionFilter to globally trap custom exceptions
    - Create ExceptionFilter implementing IExceptionFilter to capture unhandled pipeline errors.
    - Map custom TableReserveException types to their respective HTTP StatusCodes and encapsulate error collections into the unified ErrorResponse schema.
    - Gracefully catch native exceptions, forcing HTTP 500 status codes masked by a generic UNKNOWN_ERROR message.

---
+++ src/back/TableReserve.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TableReserve.Communication.Responses;
+using TableReserve.Exception;
+using TableReserve.Exception.ExceptionsBase;
+
+namespace TableReserve.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is TableReserveException tableReserveException)
+        {
+            context.HttpContext.Response.StatusCode = (int)tableReserveException.StatusCode();
+
+            context.Result = new ObjectResult(new ErrorResponse(tableReserveException.GetErrorMessages()));
+        }
+        else
+        {
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            context.Result = new ObjectResult(new ErrorResponse(MessagesExceptionResource.UNKNOWN_ERROR));
+        }
+    }
+}
