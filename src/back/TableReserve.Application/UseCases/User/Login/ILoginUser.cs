e6c85dcd478567f5546aad084ba96e8a767af932
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-23 11:21:45 -0300

    feat(application): implement LoginUser use case for user authentication
    - Create ILoginUser interface and LoginUser implementation to handle credentials validation.
    - Query dynamic active profiles via IUserReader using the incoming request email.
    - Integrate IPasswordHasher to securely verify password structures against stored Argon2id hashes.
    - Throw a custom InvalidLoginException if the user is missing or credentials mismatch to prevent enumeration attacks.
    - Generate and return a structured RegisteredUserResponse containing the JWT access token.

---
+++ src/back/TableReserve.Application/UseCases/User/Login/ILoginUser.cs
@@ -0,0 +1,9 @@
+﻿using TableReserve.Communication.Requests;
+using TableReserve.Communication.Responses;
+
+namespace TableReserve.Application.UseCases.User.Login;
+
+public interface ILoginUser
+{
+    Task<RegisteredUserResponse> Execute(LoginUserRequest request, CancellationToken cancellationToken);
+}
---
+++ src/back/TableReserve.Application/UseCases/User/Login/LoginUser.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using TableReserve.Communication.Requests;
+using TableReserve.Communication.Responses;
+using TableReserve.Domain.Repositories.User;
+using TableReserve.Domain.Security.PasswordHashing;
+using TableReserve.Domain.Security.Tokens;
+using TableReserve.Exception.ExceptionsBase;
+
+namespace TableReserve.Application.UseCases.User.Login;
+
+public class LoginUser : ILoginUser
+{
+    private readonly IPasswordHasher _passwordHasher;
+    private readonly IUserReader _userReader;
+    private readonly IAccessTokenGenerator _accessTokenGenerator;
+
+    public LoginUser(IPasswordHasher passwordHasher, IUserReader userReader, IAccessTokenGenerator accessTokenGenerator)
+    {
+        _passwordHasher = passwordHasher;
+        _userReader = userReader;
+        _accessTokenGenerator = accessTokenGenerator;
+    }
+    public async Task<RegisteredUserResponse> Execute(LoginUserRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _userReader.GetByEmail(request.Email, cancellationToken) ?? throw new InvalidLoginException();
+        
+        var isPasswordValid = _passwordHasher.VerifyPassword(request.Password, user.Password);
+
+        if(!isPasswordValid)
+            throw new InvalidLoginException();
+
+        return new RegisteredUserResponse
+        {
+            Name = user.Name,
+            Tokens = new TokensResponse
+            {
+                AccessToken = _accessTokenGenerator.Generate(user),
+            }
+        };
+    }
+}
