e2b0caf601e095ab9dfb251ff00c4435d100ec4a
GuilhAndrad <wilhelmandrad@gmail.com>
2026-09-23 13:56:34 -0300

    feat(application): implement GetUser use case to retrieve logged user profile
    - Create IGetUser interface and GetUser implementation.
    - Integrate ILoggedUser to dynamically extract the current authenticated entity context from the authorization claims.
    - Map domain properties to return a clean, un-entangled UserResponse model.

---
+++ src/back/TableReserve.Application/UseCases/User/GetUser/GetUser.cs
@@ -0,0 +1,20 @@
+﻿using TableReserve.Communication.Responses;
+using TableReserve.Domain.Repositories.User;
+
+namespace TableReserve.Application.UseCases.User.GetUser;
+
+public class GetUser : IGetUser
+{
+    private readonly ILoggedUser _loggedUser;
+    public GetUser(ILoggedUser loggedUser) => _loggedUser = loggedUser;
+    public async Task<UserResponse> Execute(CancellationToken cancellationToken)
+    {
+        var loggedUser = await _loggedUser.Get(cancellationToken);
+
+        return new UserResponse
+        {
+            Name = loggedUser.Name,
+            Email = loggedUser.Email
+        };
+    }
+}
---
+++ src/back/TableReserve.Application/UseCases/User/GetUser/IGetUser.cs
@@ -0,0 +1,8 @@
+﻿using TableReserve.Communication.Responses;
+
+namespace TableReserve.Application.UseCases.User.GetUser;
+
+public interface IGetUser
+{
+    Task<UserResponse> Execute(CancellationToken cancellationToken);
+}
